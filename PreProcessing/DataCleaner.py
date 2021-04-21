f9de032cc5e2b2923418afca47264d79fc5f6013
RaoulRoest <raoul.roest@upcmail.nl>
2021-04-21 13:29:04 +0200

    Fix test for cleaner and update cleaner

--- PreProcessing/DataCleaner.py
+++ PreProcessing/DataCleaner.py
@@ -3,14 +3,16 @@ import pandas as pd
 class DataCleaner():
     
     def get_ids_to_remove(self, dfOrig, dfMonthly):
-        ids_fico = self._get_ids(dfOrig, dfOrig["fico"] != 9999)
-        ids_cltv = self._get_ids(dfOrig, dfOrig["cltv"] != 999)
-        ids_dti = self._get_ids(dfOrig, dfOrig["dti"] != 999)
-        ids_ltv = self._get_ids(dfOrig, dfOrig["ltv"] != 999)
-        ids_ppmt_pnlty = self._get_ids(dfOrig, dfOrig["ppmt_pnlty"] != "")
-        ids_prod_type = self._get_ids(dfOrig, dfOrig["prod_type"] != "_")
+        ids_fico = self._get_ids(dfOrig, dfOrig["fico"] == 9999)
+        ids_cltv = self._get_ids(dfOrig, dfOrig["cltv"] == 999)
+        ids_dti = self._get_ids(dfOrig, dfOrig["dti"] == 999)
+        ids_ltv = self._get_ids(dfOrig, dfOrig["ltv"] == 999)
+        ids_prod_type = self._get_ids(dfOrig, dfOrig["prod_type"] == "_")
         
-        ids_foreclosure_remove = self._get_ids(dfMonthly, dfMonthly["cd_zero_bal"] > 0)
+        dfOrig["ppmt_pnlty"] = dfOrig["ppmt_pnlty"].astype(str)
+        ids_ppmt_pnlty = self._get_ids(dfOrig, (dfOrig["ppmt_pnlty"].str == "") | (dfOrig["ppmt_pnlty"].str.lower() == "nan"))
+        
+        ids_foreclosure_remove = self._get_ids(dfMonthly, dfMonthly["cd_zero_bal"] > 1)
                 
         ids = list(set(ids_fico + ids_cltv + ids_dti + ids_ltv + ids_ppmt_pnlty + ids_prod_type + ids_foreclosure_remove))
         
--- Tests/UnitTests/DataCleaner_test.py
+++ Tests/UnitTests/DataCleaner_test.py
@@ -3,17 +3,48 @@ import os
 cleanerPath = os.path.join(os.path.dirname(__file__), "..", "..", "PreProcessing")
 sys.path.append(cleanerPath)
 
-from DataCleaner import DataCleaner 
+import pytest
+import pandas as pd 
+import numpy as np
 
-from DataLoader import DataLoader
+# Custom class
+from DataCleaner import DataCleaner
+import DataInformation as di 
 
-dl = DataLoader()
-dc = DataCleaner()
+def assert_items_in_list(l1, l2):
+    assert set(l1) == set(l2)
 
-dfOrig = dl.load_origination_data_file(2020)
-dfMonthly = dl.load_monthly_performance_data_file(2020)
+def test_DataCleaner_should_remove_unclean_data():
+    # Initialize
+    dfOrig,  dfMonthly = get_test_data()
+    sut = DataCleaner()
+    
+    expected_loans = ["F113Q1000314"]
+        
+    # Act
+    resultOrig, resultMonthly = sut.clean_data(dfOrig, dfMonthly)
+    
+    # Assert
+    resultOrig_loans = resultOrig.id_loan.unique().tolist()
+    resultMonthly_loans = resultMonthly.id_loan.unique().tolist()
+    
+    assert_items_in_list(resultMonthly_loans, expected_loans)
+    assert_items_in_list(resultOrig_loans, expected_loans)
+    
+def get_test_data():
+    curDir = os.path.dirname(__file__)
+    origFilePath = os.path.join(curDir, "..", "TestData", "OrigTestFile.xlsx")
+    monthlyFilePath = os.path.join(curDir, "..", "TestData", "MonthlyTestFile.xlsx")
 
-dfMonthlyClean, dfOrigClean = dc.clean_data(dfOrig, dfMonthly)
+    headersOrig = di.get_origination_data_headers()
+    dtypesOrig = di.get_orig_dtypes()
+    typeDictOrig = {k : v for k, v in zip(headersOrig, dtypesOrig)}
+        
+    headersMonthly = di.get_monthly_performance_headers()
+    dtypesMonthly = di.get_monthly_dtypes()
+    typeDictMonthly = {k : v for k,v in zip(headersMonthly, dtypesMonthly)}
 
-all_years = range(2013, 2021)
-print([y for y in all_years])
+    dfOrig = pd.read_excel(origFilePath, sheet_name="test", dtype=typeDictOrig)
+    dfMonthly = pd.read_excel(monthlyFilePath, sheet_name="test", dtype=typeDictMonthly)
+    
+    return dfOrig, dfMonthly
