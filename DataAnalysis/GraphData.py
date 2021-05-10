cfcad0f4a348dbbdfd05f758419345083c23a2d8
SebastiaanGit <1sebastiaan1@gmail.com>
2021-05-10 18:46:29 +0200

    Plotting with new prepayment functions

--- DataAnalysis/GraphData.py
+++ DataAnalysis/GraphData.py
@@ -205,3 +205,225 @@ Run main function/script
 """
 main(years, plotOrigFeatures, plotMonthlyFeatures, scatterOrigFeatures, boxplotOrigFeatures)
 logger.info("Finished calculations")
+
+"""
+New Part
+"""
+def Complement(list1, list2):
+    lst = []
+    for i in list1:
+        if i not in list2:
+            lst.append(i)
+    return lst
+
+def hist_feature2(df1,df2, feature, bins=100, max_ticks=20,colour="blue",month=False):
+    dfGraph1 = df1[feature].copy()
+    dfGraph2 = df2[feature].copy()
+    
+    fig, ax = plt.subplots()
+    dfGraph1.hist(ax=ax, grid=False, xrot=90, bins=bins,alpha=0.3,color="blue")
+    dfGraph2.hist(ax=ax, grid=False, xrot=90, bins=bins,alpha=0.3,color="red")
+    plt.legend(["No PrePayment","PrePayment"])
+    xloc = plt.MaxNLocator(max_ticks)
+    ax.xaxis.set_major_locator(xloc)
+    if month==False:
+        gh.set_plot_params(ax, f"Histogram of {feature} of the orig data", feature)
+    else:
+        gh.set_plot_params(ax, f"Histogram of {feature} of the monthly data", feature)
+    return fig
+
+def boxplot_features2(df1,df2, featureList):
+    fig, (ax1,ax2) = plt.subplots(1,2)
+    toPlot, notToPlot = check_dtypes(df1, featureList)
+    
+    if toPlot:
+        df1.boxplot(column=toPlot, ax=ax1)
+        df2.boxplot(column=toPlot, ax=ax2)
+    else:
+        logger.warning("THERE WERE NO FEATURES TO PLOT.", level=1)
+        
+    if notToPlot:
+        featuresNotPlotted = "----".join(notToPlot)
+        logger.warning(f"THE FOLLOWING FEATURES WERE NOT PLOTTED :: {featuresNotPlotted}", level=1)
+    
+    gh.set_plot_params(ax1,
+                       title="Boxplot of several features",
+                       xlabel="feature name")
+    return fig
+
+from PrepaymentInfoProvider import calculate_prepayment_info
+
+def Plot_Full_PrePayment():
+    years = [2013]#[2013,2014,2015,2016,2017,2018,2019,2020]
+    loader = DataLoader()
+    dfOrig, dfMonthly = loader.get_data_set(years)
+    
+    dfPP=calculate_prepayment_info(dfOrig,dfMonthly)
+    
+    
+    PPdfPP=dfPP.loc[dfPP["prepayment_type"] == "FullPrepayment"]
+    loanids=dfOrig.index
+    #NFPPdfFPP=dfFPP.loc[dfFPP["FlagFullPrepayment"] == True]
+    loanidsPP=PPdfPP.index
+    loanidsNPP=Complement(loanids,loanidsPP)
+    PPnewdfOrig=dfOrig.loc[loanidsPP]
+    NPPnewdfOrig=dfOrig.loc[loanidsNPP]
+    
+    for column in plotOrigFeatures["features"]:
+        fig = hist_feature2(NPPnewdfOrig,PPnewdfOrig, column)
+        fig.tight_layout()
+        plt.show()
+        #gh.save_plot(fig, f"{column}_histogram_{years}", "HistogramsOriginateFeatures")
+        #plt.close()
+    
+    for column in boxplotOrigFeatures["features"]:
+        fig = boxplot_features2(NPPnewdfOrig,PPnewdfOrig,[column])
+        fig.tight_layout()
+        plt.show()
+        #gh.save_plot(fig, f"Boxplot_of_{column}_{years}", "BoxplotOrigData")
+        #plt.close()
+        
+    
+def Plot_Partial_PrePayment():    
+    years = [2013]#[2013,2014,2015,2016,2017,2018,2019,2020]
+    loader = DataLoader()
+    dfOrig, dfMonthly = loader.get_data_set(years)
+    
+    dfPP=calculate_prepayment_info(dfOrig,dfMonthly)
+    
+    
+    PPdfPP=dfPP.loc[dfPP["prepayment_type"] == "PartialPrepayment"]
+    loanids=dfOrig.index
+    #NFPPdfFPP=dfFPP.loc[dfFPP["FlagFullPrepayment"] == True]
+    loanidsPP=PPdfPP.index
+    loanidsNPP=Complement(loanids,loanidsPP)
+    PPnewdfOrig=dfOrig.loc[loanidsPP]
+    NPPnewdfOrig=dfOrig.loc[loanidsNPP]
+    
+    for column in plotOrigFeatures["features"]:
+        fig = hist_feature2(NPPnewdfOrig,PPnewdfOrig, column)
+        fig.tight_layout()
+        plt.show()
+        #gh.save_plot(fig, f"{column}_histogram_{years}", "HistogramsOriginateFeatures")
+        #plt.close()
+        
+        
+    for column in boxplotOrigFeatures["features"]:
+        fig = boxplot_features2(NPPnewdfOrig,PPnewdfOrig,[column])
+        fig.tight_layout()
+        plt.show()
+        #gh.save_plot(fig, f"Boxplot_of_{column}_{years}", "BoxplotOrigData")
+        #plt.close()
+    
+    
+def Plot_Partial_and_Full_PrePayment():  
+    years = [2013]#[2013,2014,2015,2016,2017,2018,2019,2020]
+    loader = DataLoader()
+    dfOrig, dfMonthly = loader.get_data_set(years)
+    
+    dfPP=calculate_prepayment_info(dfOrig,dfMonthly)
+    
+    
+    PPdfPP=dfPP.loc[dfPP["prepayment_type"] == "FullPrepayment"]
+    extra=dfPP.loc[dfPP["prepayment_type"] == "PartialPrepayment"]
+    PPdfPP=PPdfPP.append(extra)
+    loanids=dfOrig.index
+    #NFPPdfFPP=dfFPP.loc[dfFPP["FlagFullPrepayment"] == True]
+    loanidsPP=PPdfPP.index
+    loanidsNPP=Complement(loanids,loanidsPP)
+    PPnewdfOrig=dfOrig.loc[loanidsPP]
+    NPPnewdfOrig=dfOrig.loc[loanidsNPP]
+    
+    for column in plotOrigFeatures["features"]:
+        fig = hist_feature2(NPPnewdfOrig,PPnewdfOrig, column)
+        fig.tight_layout()
+        plt.show()
+        #gh.save_plot(fig, f"{column}_histogram_{years}", "HistogramsOriginateFeatures")
+        #plt.close()
+        
+    for column in boxplotOrigFeatures["features"]:
+        fig = boxplot_features2(NPPnewdfOrig,PPnewdfOrig,[column])
+        fig.tight_layout()
+        plt.show()
+        #gh.save_plot(fig, f"Boxplot_of_{column}_{years}", "BoxplotOrigData")
+        #plt.close()
+        
+def Get_months_PrePayment(df):
+    """Df as returned by prepayment_info(dfOrig,dfMonthly)"""
+    NoPP=df.loc[df["prepayment_type"] == "No Prepayment"]
+    FullPP=df.loc[df["prepayment_type"] == "FullPrepayment"]
+    PartialPP=df.loc[df["prepayment_type"] == "PartialPrepayment"]
+    MonthNoPP=NoPP["orig_loan_term"]-NoPP["mths_remng"]
+    MonthFullPP=FullPP["orig_loan_term"]-FullPP["mths_remng"]
+    MonthPartialPP=PartialPP["orig_loan_term"]-PartialPP["mths_remng"]
+    return MonthNoPP,MonthFullPP,MonthPartialPP
+
+def Split_data_PrePayment(df):
+    """df as returned by prepayment_info(dfOrig,dfMonthly)"""
+    NoPP=df.loc[df["prepayment_type"] == "No Prepayment"]
+    FullPP=df.loc[df["prepayment_type"] == "FullPrepayment"]
+    PartialPP=df.loc[df["prepayment_type"] == "PartialPrepayment"]
+    return NoPP,FullPP,PartialPP
+
+def boxplot_features4(df1,df2,df3,df4, featureList):
+    fig, (ax1,ax2,ax3,ax4) = plt.subplots(1,4)
+    toPlot, notToPlot = check_dtypes(df1, featureList)
+    
+    if toPlot:
+        df1.boxplot(column=toPlot, ax=ax1)
+        df2.boxplot(column=toPlot, ax=ax2)
+        df3.boxplot(column=toPlot, ax=ax3)
+        df4.boxplot(column=toPlot, ax=ax4)
+    else:
+        logger.warning("THERE WERE NO FEATURES TO PLOT.", level=1)
+        
+    if notToPlot:
+        featuresNotPlotted = "----".join(notToPlot)
+        logger.warning(f"THE FOLLOWING FEATURES WERE NOT PLOTTED :: {featuresNotPlotted}", level=1)
+    
+    gh.set_plot_params(ax1,
+                       title="Boxplot of several features",
+                       xlabel="feature name")
+    return fig
+
+def Plot_PrePayment_comparison(df):
+    df["timescale"]=df["mths_remng"]/df["orig_loan_term"]
+    df["payscale"]=df["current_upb"]/df["orig_upb"]
+    NoPP,FullPP,PartialPP=Split_data_PrePayment(df)
+    # for column in plotMonthlyFeatures["features"]:
+    #     fig = boxplot_features4(NPPnewdfOrig,PPnewdfOrig,[column])
+    #     fig.tight_layout()
+    #     plt.show()
+    fig=boxplot_features4(NoPP,FullPP,PartialPP,df,["timescale","payscale"])
+    fig.tight_layout()
+    plt.show()
+    return NoPP
+
+def Yearmonths(yyyymm,m):
+    yyyy=yyyymm[:4]
+    mm=yyyymm[4:]
+    y=int(m/12)
+    months=m%12
+    newyear=int(yyyy)-y
+    if int(months)<int(mm):
+        newmonths=int(mm)-months
+    else:
+        newyear-=1
+        newmonths=int(mm)+12-months
+    date=str(newyear)+str(newmonths).zfill(2)
+    return date
+
+def MultipleYearMonths(yyyymm,m):
+    dates=np.zeros(len(yyyymm))
+    for i in range(len(yyyymm)):
+        dates[i]=Yearmonths(yyyymm[i],m[i])
+    return dates
+        
+        
+        
+    
+    
+        
+        
+    
+
